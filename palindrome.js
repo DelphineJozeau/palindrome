10d2fb0cdae592315808b175c3a733173613e43e
Flaya <delphine.jozeau@gmail.com>
2025-03-14 21:29:23 +0100

    js

--- palindrome.js
+++ palindrome.js
@@ -27,32 +27,13 @@ function isValidDate(date) {
 
     //JOUR
     if (jour > 0 && jour <= 31) {
-        let jourMax = 0
-        if (mois == 1 || mois == 3 || mois == 5 || mois == 7 || mois == 8 || mois == 10 || mois == 12) {
-            jourMax = 31;
-        }
-
-        else if (mois == 4 || mois == 6 || mois == 9 || mois == 11) {
-            jourMax = 30;
-        }
-
-        else if (mois == 2) {
-
-            if (annee % 4 == 0) {
-                jourMax = 29;
-            }
-            else { jourMax = 28; }
-
-        }
-
-        else {
-            console.log("Jour invalide !");
-            return false;
-        }
+   let jourMax = maxDayInMonth(mois);
 
         if (jour > jourMax) {
+            
             console.log("jour invalide !")
             return false
+
         } else {
             console.log("jour valide");
         }
@@ -67,11 +48,32 @@ function isValidDate(date) {
     return true;
 };
 
+function maxDayInMonth(mois) {
+    let jourMax = 0
+    if (mois == 1 || mois == 3 || mois == 5 || mois == 7 || mois == 8 || mois == 10 || mois == 12) {
+        jourMax = 31;
+    }
+
+    else if (mois == 4 || mois == 6 || mois == 9 || mois == 11) {
+        jourMax = 30;
+    }
+
+    else if (mois == 2) {
+
+        if (annee % 4 == 0) {
+            jourMax = 29;
+        }
+        else { jourMax = 28; }
+    }
+    return jourMax
+};
+
 function isPalindrome(date) {
 
     let part = date.split("/");
+
     let part1 = part[0] + part[1];
-    let part2 = part[2]
+    let part2 = inverserChaine(part[2]);
 
     if (part1 == part2) {
         console.log("Palindrome")
@@ -84,7 +86,29 @@ function isPalindrome(date) {
 
 };
 
+function inverserChaine(str) {
+    let inverse = "";
+    for (let index = str.length - 1; index >= 0; index--) { inverse = inverse + str[index]; };
+    return inverse;
+};
+
+
+
+function getNextPalindromes(x) {
+
+    //tester la date
+    if (isValidDate(date)) {
+
+    }
+    else { console.log("date invalide !!") }
+
+    //tester la date valide 
+
+};
+
+
 console.log(isValidDate("25/04/3067"));
+
 console.log(isPalindrome("22/02/2022"));
 
 
